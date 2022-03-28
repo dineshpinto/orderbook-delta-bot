9dcd61f1829f6f72aaaf73812b4d14b511a400fc
dineshpinto <dkpinto95@gmail.com>
2022-03-28 13:47:21 +0200

    Added logging functionality, updated gitignore

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::{
     error::Error,
     fs::{File, OpenOptions},
+    io::BufReader,
     path::Path,
     thread,
     time::Duration
@@ -8,10 +9,12 @@ use std::{
 
 use chrono::prelude::*;
 use csv::Writer;
+use env_logger::{Builder, Target, WriteStyle};
 use ftx::{
     options::{Endpoint, Options},
     rest::{GetFuture, GetOrderBook, Rest},
 };
+use log::{debug, info, LevelFilter, trace, warn};
 use rust_decimal::prelude::ToPrimitive;
 use serde::{Deserialize, Serialize};
 use serde_json;
@@ -19,6 +22,7 @@ use ta::{indicators::BollingerBands, Next};
 
 #[derive(Serialize, Deserialize)]
 struct SettingsFile {
+    market_name: String,
     time_delta: u64,
     bb_period: usize,
     bb_std_dev: f64,
@@ -45,18 +49,28 @@ async fn main() {
     // Load configuration file
     let settings_filepath = Path::new("settings.json");
     let settings_file = File::open(settings_filepath).expect("Config file not found");
+    let reader = BufReader::new(settings_file);
     let settings: SettingsFile =
-        serde_json::from_reader(settings_file).expect("Error when reading config json");
+        serde_json::from_reader(reader).expect("Error when reading config json");
 
-    println!(
-        "Settings file loaded from {:?}. time_delta={:?}, bb_period={:?}, bb_std_dev={:?}, \
-        orderbook_depth={:?}",
-        settings_filepath,
+    let mut builder= Builder::new();
+    builder
+        .filter(None, LevelFilter::Info)
+        .write_style(WriteStyle::Always)
+        .target(Target::Stdout)
+        .init();
+
+    info!("Settings file loaded from {:?}.", settings_filepath);
+    info!(
+        "market_name={:?}, time_delta={:?}, bb_period={:?}, bb_std_dev={:?}, orderbook_depth={:?}",
+        String::from(&settings.market_name),
         settings.time_delta,
         settings.bb_period,
         settings.bb_std_dev,
         settings.orderbook_depth
     );
+    info!("Setting trigger in {:?} iterations (approx {:?}s)...",
+        settings.bb_period, settings.bb_period * settings.time_delta.to_usize().unwrap());
 
     // Set up connection to FTX API
     let api = Rest::new(
@@ -71,12 +85,12 @@ async fn main() {
     loop {
         count += 1;
         let order_book = api.request(
-            GetOrderBook { market_name: "BTC-PERP".to_string(), depth: Option::from(settings.orderbook_depth) }
+            GetOrderBook { market_name: String::from(&settings.market_name), depth: Option::from(settings.orderbook_depth) }
         ).await;
 
         let order_book = match order_book {
             Err(e) => {
-                println!("Error: {:?}", e);
+                warn!("Error: {:?}", e);
                 continue
             },
             Ok(o) => o
@@ -88,23 +102,22 @@ async fn main() {
         let bb_lower = out.lower;
         let bb_upper = out.upper;
 
-        println!("perp_delta={:.2}, bb_lower={:.2}, bb_upper={:.2}", perp_delta, bb_lower, bb_upper);
+        debug!("perp_delta={:.2}, bb_lower={:.2}, bb_upper={:.2}", perp_delta, bb_lower, bb_upper);
 
         if count > settings.bb_period {
             if count == settings.bb_period + 1 {
-                println!("Trigger is now set...")
+                warn!("Trigger is now set...")
             }
 
             if perp_delta > bb_upper || perp_delta < bb_lower {
                 let btc_price = api.request(
-                    GetFuture { future_name: "BTC-PERP".to_string() }
+                    GetFuture { future_name: String::from(&settings.market_name) }
                 ).await;
-                let utc_time: DateTime<Utc> = Utc::now();
 
 
                 let btc_price = match btc_price {
                     Err(e) => {
-                        println!("Error: {:?}", e);
+                        warn!("Error: {:?}", e);
                         continue
                     },
                     Ok(o) => o
@@ -116,15 +129,14 @@ async fn main() {
                 if perp_delta > bb_upper {
                     price = btc_price.ask.unwrap().to_f64().unwrap();
                     position = "short".to_string();
-                    println!("{:?} Perp delta above upper bb, going {} at {:.2}",
-                             utc_time, position, price);
+                    warn!("Perp delta above upper bb, going {} at {:.2}", position, price);
                 } else if perp_delta < bb_lower {
                     price = btc_price.bid.unwrap().to_f64().unwrap();
                     position = "long".to_string();
-                    println!("{:?} Perp delta below lower bb, going {} at {:.2}",
-                             utc_time, position, price);
+                    warn!("Perp delta below lower bb, going {} at {:.2}", position, price);
                 }
                 // Write the positions to a csv
+                let utc_time: DateTime<Utc> = Utc::now();
                 write_to_csv(utc_time.to_string(), price.to_string(), position);
             }
         }
