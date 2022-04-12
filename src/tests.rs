b3d1e4fe7401b373c017ca894309c076077c740b
dineshpinto <dkpinto95@gmail.com>
2022-04-13 00:42:24 +0200

    Cleaned up tests

--- src/tests.rs
+++ src/tests.rs
@@ -1,18 +1,19 @@
 #[cfg(test)]
 mod test_helpers{
+    use crate::helpers::{convert_increment_to_precision, read_settings, SettingsFile, write_to_csv};
 
     #[test]
     fn test_convert_increment_to_precision() {
         let value = rust_decimal::prelude::FromPrimitive::from_f64(0.1).unwrap();
-        let precision = crate::helpers::convert_increment_to_precision(value);
+        let precision = convert_increment_to_precision(value);
         assert_eq!(precision, 1 as u32);
     }
 
     #[test]
     fn test_write_to_csv() {
         // Create a test file
-        let filename = "write_to_csv_test.csv";
-        crate::helpers::write_to_csv(
+        let filename = "test_write_to_csv.csv";
+        write_to_csv(
             filename,
             rust_decimal::Decimal::from(10 as i64),
             rust_decimal::Decimal::from(10 as i64),
@@ -35,8 +36,8 @@ mod test_helpers{
     #[test]
     fn test_read_settings() {
         // Create a test file
-        let filename = "read_settings_test.json";
-        let data =  crate::helpers::SettingsFile {
+        let filename = "test_read_settings.json";
+        let data =  SettingsFile {
             market_name: "BTC-USD".to_string(),
             time_delta: 1,
             bb_period: 10,
@@ -52,7 +53,7 @@ mod test_helpers{
             &std::fs::File::create(filename).unwrap(), &data).unwrap();
 
         // Verify the test file, and delete it
-        let settings = crate::helpers::read_settings(filename);
+        let settings = read_settings(filename);
         assert_eq!(settings.time_delta, 1 as u64);
         assert_eq!(settings.bb_period, 10 as usize);
         assert_eq!(settings.bb_std_dev, 0 as f64);
